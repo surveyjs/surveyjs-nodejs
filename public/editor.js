f65779d7cc771d7b324c83a196e2d262b442c9cf
tsv2013 <tsv2013@users.noreply.github.com>
2017-12-21 17:10:01 +0300

    Changed port to 3000 and ran "prettifier" by accident

--- public/editor.js
+++ public/editor.js
@@ -1,29 +1,36 @@
 function getParams() {
-    var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    var result = {};
-    url.forEach(function(item) {
-        var param = item.split("=");
-        result[param[0]] = param[1];
-    });
-    return result;
+  var url = window.location.href
+    .slice(window.location.href.indexOf("?") + 1)
+    .split("&");
+  var result = {};
+  url.forEach(function(item) {
+    var param = item.split("=");
+    result[param[0]] = param[1];
+  });
+  return result;
 }
-  
-Survey.dxSurveyService.serviceUrl = "http://localhost:8000";
+
+Survey.dxSurveyService.serviceUrl = "http://localhost:3000";
 var accessKey = "";
 var editor = new SurveyEditor.SurveyEditor("editor");
 var surveyId = getParams()["id"];
 editor.loadSurvey(surveyId);
-editor.saveSurveyFunc = function (saveNo, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', Survey.dxSurveyService.serviceUrl + '/changeJson?accessKey=' + accessKey);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onload = function () {
-        var result = xhr.response ? JSON.parse(xhr.response) : null;
-        if(xhr.status === 200) {
-            callback(saveNo, true)
-        }
-    };
-    xhr.send(JSON.stringify({ Id: surveyId, Json: editor.text, Text: editor.text }));
+editor.saveSurveyFunc = function(saveNo, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "POST",
+    Survey.dxSurveyService.serviceUrl + "/changeJson?accessKey=" + accessKey
+  );
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onload = function() {
+    var result = xhr.response ? JSON.parse(xhr.response) : null;
+    if (xhr.status === 200) {
+      callback(saveNo, true);
+    }
+  };
+  xhr.send(
+    JSON.stringify({ Id: surveyId, Json: editor.text, Text: editor.text })
+  );
 };
 editor.isAutoSave = true;
 editor.showState = true;
--- public/survey.js
+++ public/survey.js
@@ -1,39 +1,40 @@
 function getParams() {
-    var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    var result = {};
-    url.forEach(function(item) {
-        var param = item.split("=");
-        result[param[0]] = param[1];
-    });
-    return result;
+  var url = window.location.href
+    .slice(window.location.href.indexOf("?") + 1)
+    .split("&");
+  var result = {};
+  url.forEach(function(item) {
+    var param = item.split("=");
+    result[param[0]] = param[1];
+  });
+  return result;
 }
-  
+
 function init() {
+  Survey.dxSurveyService.serviceUrl = "http://localhost:3000";
+  Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
+  Survey.Survey.cssType = "bootstrap";
 
-    Survey.dxSurveyService.serviceUrl = "http://localhost:8000";
-    Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
-    Survey.Survey.cssType = "bootstrap";
+  var surveyId = getParams()["id"];
+  var model = new Survey.Model({ surveyId: surveyId, surveyPostId: surveyId });
+  window.survey = model;
+  model.render("surveyElement");
 
-    var surveyId = getParams()["id"];
-    var model = new Survey.Model({ surveyId: surveyId, surveyPostId: surveyId });
-    window.survey = model;
-    model.render("surveyElement");
-    
-    // // Load survey by id from url
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('GET', "http://localhost:8000" + '/survey?surveyId=' + surveyId);
-    // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    // xhr.onload = function () {
-    //     var result = JSON.parse(xhr.response);
-    //     if(!!result) {
-    //         var surveyModel = new Survey.Model(result);
-    //         window.survey = surveyModel;
-    //         ko.cleanNode(document.getElementById("surveyElement"));
-    //         document.getElementById("surveyElement").innerText = "";
-    //         surveyModel.render("surveyElement");
-    //     }
-    // };
-    // xhr.send();
+  // // Load survey by id from url
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', "http://localhost:8000" + '/survey?surveyId=' + surveyId);
+  // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // xhr.onload = function () {
+  //     var result = JSON.parse(xhr.response);
+  //     if(!!result) {
+  //         var surveyModel = new Survey.Model(result);
+  //         window.survey = surveyModel;
+  //         ko.cleanNode(document.getElementById("surveyElement"));
+  //         document.getElementById("surveyElement").innerText = "";
+  //         surveyModel.render("surveyElement");
+  //     }
+  // };
+  // xhr.send();
 }
 
 init();
