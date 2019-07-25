aa56f352a28d4dfe4534d93d17f278d24b5274ef
Paul Zhang <paul.zhang4156@gmail.com>
2019-07-25 16:47:03 +1000

    Moved docker-compose.yml out

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Sample NodeJS backend for SurveyJS: Survey library and Survey Creator
 
+<!-- TOC depthFrom:2 -->
 
-### Disclaimer
+- [Disclaimer](#disclaimer)
+- [Getting started](#getting-started)
+  - [Use Postgresql database as backend](#use-postgresql-database-as-backend)
+- [Links](#links)
+
+<!-- /TOC -->
+
+## Disclaimer
 This demo illustrates how to integrate SurveyJS libraries with NodeJS backend. This demo doesn't cover all of real survey service application aspects, such as authentication, authorization, user management, access levels and different security issues. These aspects are covered by backend-specific articles, forums and documentation. This demo demos is just intergration one and can't be used as a real service.
 
-## [SurveyJS Home Page](https://surveyjs.io/Examples/Service/)
-
-## [Live Online Survey and Survey Creator Demo](https://surveyjs-nodejs.herokuapp.com/)
-
-
-### Getting started
+## Getting started
 - Install NodeJS
 - Clone this repository in the `surveyjs-nodejs` folder (and make it current directory via `cd surveyjs-nodejs`)
 - Install npm packages via the `npm i` command
@@ -21,9 +24,13 @@ If everything is ok, you should see project home page with list of available sur
 - You can continue with Survey Creator via `Edit` page, change the survey and store survey JSON to the custom service.
 - Saved survey results are available via `Results` link.
 
-
-### Adding Postgresql database to the backend
+### Use Postgresql database as backend
 - Install [docker](https://www.docker.com/) and [docker-compose](https://docs.docker.com/compose/install/) on your computer
-- `cd docker`
-- `docker-compose up`
+- `docker volume create pgdata`
+- `docker-compose up -d`
+- In adminer, run SQL command in docker/postgresql-db/initdb/init.sql
 - Change db adapter from InMemoryDBAdapter to Postgresql one.
+
+## Links
+* [SurveyJS Home Page](https://surveyjs.io/Examples/Service/)
+* [Live Online Survey and Survey Creator Demo](https://surveyjs-nodejs.herokuapp.com/)
--- index.js
+++ index.js
@@ -27,74 +27,75 @@ function sendJsonResult(res, obj) {
   res.send(JSON.stringify(obj));
 }
 
-app.get("/getActive", function(req, res) {
+app.get("/getActive", function (req, res) {
   var db = getDBAdapter(req);
-  db.getSurveys(function(result) {
+  db.getSurveys(function (result) {
     sendJsonResult(res, result);
   });
 });
 
-app.get("/getSurvey", function(req, res) {
+app.get("/getSurvey", function (req, res) {
   var db = getDBAdapter(req);
   var surveyId = req.query["surveyId"];
-  db.getSurvey(surveyId, function(result) {
+  db.getSurvey(surveyId, function (result) {
     sendJsonResult(res, result);
   });
 });
 
-app.get("/changeName", function(req, res) {
+app.get("/changeName", function (req, res) {
   var db = getDBAdapter(req);
   var id = req.query["id"];
   var name = req.query["name"];
-  db.changeName(id, name, function(result) {
+  db.changeName(id, name, function (result) {
     sendJsonResult(res, result);
   });
 });
 
-app.get("/create", function(req, res) {
+app.get("/create", function (req, res) {
   var db = getDBAdapter(req);
   var name = req.query["name"];
-  db.addSurvey(name, function(result) {
+  db.addSurvey(name, function (result) {
     sendJsonResult(res, { Name: result.name, Id: result.name });
   });
 });
 
-app.post("/changeJson", function(req, res) {
+app.post("/changeJson", function (req, res) {
   var db = getDBAdapter(req);
   var id = req.body.Id;
   var json = req.body.Json;
-  db.storeSurvey(id, json, function(result) {
+
+  db.storeSurvey(id, json, function (result) {
     sendJsonResult(res, result.json);
   });
 });
 
-app.post("/post", function(req, res) {
+app.post("/post", function (req, res) {
   var db = getDBAdapter(req);
   var postId = req.body.postId;
   var surveyResult = req.body.surveyResult;
-  db.postResults(postId, surveyResult, function(result) {
+  db.postResults(postId, surveyResult, function (result) {
     sendJsonResult(res, result.json);
   });
 });
 
-app.get("/delete", function(req, res) {
+app.get("/delete", function (req, res) {
   var db = getDBAdapter(req);
   var surveyId = req.query["id"];
-  db.deleteSurvey(surveyId, function(result) {
+  db.deleteSurvey(surveyId, function (result) {
     sendJsonResult(res, {});
   });
 });
 
-app.get("/results", function(req, res) {
+app.get("/results", function (req, res) {
   var db = getDBAdapter(req);
   var postId = req.query["postId"];
-  db.getResults(postId, function(result) {
+  db.getResults(postId, function (result) {
     sendJsonResult(res, result);
   });
 });
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Listening!");
 });
